6c98a9beca68184143586a2dc4fb2fa50240f627
notnulldev <notnulldev0@gmail.com>
2022-08-27 23:39:48 +0200

    working simple websocket server

--- websocket/go.mod
+++ websocket/go.mod
@@ -1,3 +1,7 @@
 module notnulldev.com/realtime-chat-ws-server
 
 go 1.19
+
+require github.com/google/uuid v1.3.0
+
+require github.com/gorilla/websocket v1.5.0 // indirect
--- websocket/server.go
+++ websocket/server.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+)
+
 type ChatUser struct {
 	id string
 }
@@ -8,6 +19,7 @@ type ChatMessage struct {
 	Id      string
 	Author  ChatUser
 	Content string
+	Channel string
 }
 
 type Server struct {
@@ -19,7 +31,6 @@ type Server struct {
 
 func (server *Server) Run() {
 	for {
-
 		select {
 		case user := <-server.connectClient:
 			println("Connected client with id " + user.id)
@@ -34,13 +45,74 @@ func (server *Server) Run() {
 
 func createServer() *Server {
 	return &Server{
-		id:               uuid.,
+		id:               uuid.NewString(),
 		newMessage:       make(chan ChatMessage),
 		connectClient:    make(chan ChatUser),
 		disconnectClient: make(chan ChatUser),
 	}
 }
 
+var addr = flag.String("a", "localhost:7777", "address to server websocket")
+var endpoint = flag.String("e", "/server", "endpoint ot serve websocket connection")
+
 func main() {
-	createServer()
+	logFile, err := os.Create("log.log")
+
+	if err != nil {
+		println(err.Error() + "\nCan't create log file!")
+	} else {
+		log.SetOutput(logFile)
+	}
+
+	server := createServer()
+
+	log.Println("addr:", *addr)
+	log.Println("endpoint:", *endpoint)
+
+	go server.Run()
+
+	url := url.URL{
+		Scheme: "ws",
+		Host:   *addr,
+		Path:   *endpoint,
+	}
+
+	log.Println("URL STRING: ", url.String())
+
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	http.HandleFunc(*endpoint, func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Starting server on %v", *addr)
+		wsConnection, err := upgrader.Upgrade(w, r, nil)
+
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		defer wsConnection.Close()
+
+		for {
+			msgType, msgContent, err := wsConnection.ReadMessage()
+
+			log.Printf("New connection!")
+
+			if err != nil {
+				log.Fatalln(err.Error())
+				return
+			}
+
+			log.Printf("Received new message with type: [%v] and content: [%v]", msgType, string(msgContent))
+		}
+
+	})
+
+	err = http.ListenAndServe(*addr, nil)
+
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
