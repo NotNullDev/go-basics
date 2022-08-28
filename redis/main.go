2615c405cd77ade9856e5a0e0950b94edbb9eae5
notnulldev <notnulldev0@gmail.com>
2022-08-28 20:34:57 +0200

    up

--- redis/main.go
+++ redis/main.go
@@ -2,15 +2,39 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
 
 	"github.com/notnulldev.com/go-example/redis/pubsub"
 )
 
 func main() {
 	// readwrite.ReadWriteExample(redisClient)
+
+	logFile, err := os.Create("log.log") // ~6700 req/s
+
+	if err != nil {
+		println(err)
+	}
+	defer logFile.Close()
+
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+
+	log.SetOutput(multiWriter)
+
+	before := time.Now()
+
+	defer func() {
+		after := time.Now()
+		diff := after.Sub(before)
+		log.Printf("Time passed: %v", diff)
+	}()
+
 	go pubsub.StartSubscriber("dummy")
 
-	go pubsub.StartPublisher("dummy", 100)
+	go pubsub.StartPublisher("dummy", 0)
 
 	fmt.Scanln()
 }
